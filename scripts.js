55c7006c1af189492d86e318311b08e42f40c292
neto <antonio.garciamn@gmail.com>
2021-11-18 21:05:23 -0300

    Recebendo os Quizzes da API

--- scripts.js
+++ scripts.js
@@ -1,3 +1,44 @@
-function puxarQuizzes{
-    const pormesa = axios.get()
+let idEscolhido;
+
+function puxarQuizzes(){
+  const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+  promessa.then((resposta) =>{
+    console.log(resposta);
+    for(let i=0; i< resposta.data.length; i++){
+      const quizzes = document.querySelector(".todosQuizzes .quizzes");
+      quizzes.innerHTML += `
+      <article class="quizz" onclick="escolherQuizz(this)">
+      <img src=${resposta.data[i].image}>
+      <p>${resposta.data[i].title}</p>
+      <div class="gradiente"></div>
+      <div class="id invisivel">${resposta.data[i].id}</div>
+      </article>`;
+    }
+  });
 }
+
+function escolherQuizz(sele){
+  const telaInicial = document.querySelector(".telaInicial");
+  const jogandoQuiz = document.querySelector(".jogandoQuizz");
+  telaInicial.classList.add("invisivel");
+  jogandoQuiz.classList.remove("invisivel");
+  idEscolhido = sele.querySelector(".id").innerHTML;
+  console.log("idEscolhido: "+ idEscolhido);
+  pegarQuizzEscolhido();
+}
+
+function pegarQuizzEscolhido(){
+  const promessa = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idEscolhido}`);
+  promessa.then((reposta) =>{
+    console.log(reposta);
+  });
+}
+
+
+
+
+
+
+
+puxarQuizzes();
+

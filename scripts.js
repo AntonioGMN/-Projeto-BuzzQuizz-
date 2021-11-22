2b4ff928e7b4c77d4e210c8b1af8876dbc797139
neto <antonio.garciamn@gmail.com>
2021-11-22 18:39:44 -0300

    função de criar quizz

--- scripts.js
+++ scripts.js
@@ -57,26 +57,27 @@ function pegarQuizzEscolhido(){
       <section class="pergunta nãoRespondida">
         <h1 style="background:${dados.questions[i].color};" >${dados.questions[i].title}</h1>
         <section class="respostas">
-          <article class="resposta" onclick="verificarResposta(this)">
-            <img src=${dados.questions[i].answers[n[0]].image}>
-            <p>${dados.questions[i].answers[n[0]].text}</p>
-            <div class="invisivel">${dados.questions[i].answers[n[0]].isCorrectAnswer}</div>
-          </article>
-          <article class="resposta" onclick="verificarResposta(this)">
-            <img src=${dados.questions[i].answers[n[1]].image}>
-            <p>${dados.questions[i].answers[n[1]].text}</p>
-            <div class="invisivel">${dados.questions[i].answers[n[1]].isCorrectAnswer}</div>
-          </article>
-          <article class="resposta" onclick="verificarResposta(this)">
-            <img src=${dados.questions[i].answers[n[2]].image}>
-            <p>${dados.questions[i].answers[n[2]].text}</p>
-            <div class="invisivel">${dados.questions[i].answers[n[2]].isCorrectAnswer}</div>
-          </article>
-          <article class="resposta" onclick="verificarResposta(this)">
-            <img src=${dados.questions[i].answers[n[3]].image}>
-            <p>${dados.questions[i].answers[n[3]].text}</p>
-            <div class="invisivel">${dados.questions[i].answers[n[3]].isCorrectAnswer}</div>
-          </article>
+            <article class="resposta" onclick="verificarResposta(this)">
+              <img src=${dados.questions[i].answers[n[0]].image}>
+              <p>${dados.questions[i].answers[n[0]].text}</p>
+              <div class="invisivel">${dados.questions[i].answers[n[0]].isCorrectAnswer}</div>
+            </article>
+            <article class="resposta" onclick="verificarResposta(this)">
+              <img src=${dados.questions[i].answers[n[1]].image}>
+              <p>${dados.questions[i].answers[n[1]].text}</p>
+              <div class="invisivel">${dados.questions[i].answers[n[1]].isCorrectAnswer}</div>
+            </article>
+            <article class="resposta" onclick="verificarResposta(this)">
+              <img src=${dados.questions[i].answers[n[2]].image}>
+              <p>${dados.questions[i].answers[n[2]].text}</p>
+              <div class="invisivel">${dados.questions[i].answers[n[2]].isCorrectAnswer}</div>
+            </article>
+            <article class="resposta" onclick="verificarResposta(this)">
+              <img src=${dados.questions[i].answers[n[3]].image}>
+              <p>${dados.questions[i].answers[n[3]].text}</p>
+              <div class="invisivel">${dados.questions[i].answers[n[3]].isCorrectAnswer}</div>
+            </article>
+
         </section>
       </section>`
     }
@@ -97,6 +98,7 @@ function pegarQuizzEscolhido(){
 
 function verificarResposta(sele){
   const respostas = sele.parentElement.querySelectorAll(".resposta");
+  const resposta = sele.parentElement.querySelector(".resposta");
   for(let i=0; i<4; i++){
     if(respostas[i].querySelector(".invisivel").innerHTML == "false"){
       respostas[i].classList.add("erro");
@@ -113,18 +115,19 @@ function verificarResposta(sele){
     pontuação += (100/perguntas.length);
   }
 
+  const perguntaAtual = document.querySelector(".pergunta.nãoRespondida");
+  perguntaAtual.classList.remove("nãoRespondida");
+  perguntaAtual.classList.add("Respondida");
   const proximaPergunta = document.querySelector(".pergunta.nãoRespondida");
-  proximaPergunta.classList.remove("nãoRespondida");
-  proximaPergunta.classList.add("Respondida");
+  //const respondido = document.querySelector(".pergunta.Respondida");
+  //console.log(respondido);
   setTimeout(() => {
-    const proximaPergunta = document.querySelector(".pergunta.nãoRespondida");
     if(proximaPergunta != null){
-      proximaPergunta.scrollIntoView();
+      proximaPergunta.scrollIntoView({block: "center"});
     }else{
       carregarPontuação();
     }
   },2000);
-
 }
 
 function carregarPontuação(){
@@ -144,7 +147,7 @@ function carregarPontuação(){
     }
   });
   levels.parentElement.classList.remove("invisivel");
-  levels.scrollIntoView();
+  levels.scrollIntoView({block: "center"});
 }
 
 function reiniciar(){
@@ -178,136 +181,122 @@ function voltar(){
   inicio.classList.remove("invisivel");
 }
 
-// let ultimaMensagem;
-// let nomeDeUsuario;
+function criarQuizz(){
+  const telaInicial = document.querySelector(".telaInicial");
+  telaInicial.classList.add("invisivel");
+  const tela31 = document.querySelector(".tela31");
+  tela31.classList.remove("invisivel");
+}
 
-// function scrollarAteUltimaMensagem() {
-//     const ulMensagens = document.querySelector(".mensagens-container");
-//     const liUltimaMensagem = ulMensagens.lastElementChild;
+let ultimaMensagem;
+let nomeDeUsuario;
 
-//     if (ultimaMensagem !== liUltimaMensagem) {
-//         liUltimaMensagem.scrollIntoView();
-//         ultimaMensagem = liUltimaMensagem;
-//     }
-// }
+function scrollarAteUltimaMensagem() {
+    const ulMensagens = document.querySelector(".mensagens-container");
+    const liUltimaMensagem = ulMensagens.lastElementChild;
+    if (ultimaMensagem !== liUltimaMensagem) {
+        liUltimaMensagem.scrollIntoView();
+        ultimaMensagem = liUltimaMensagem;
+    }
+}
 
-// function Mensagem(dados) {
-//     let classeMensagem = '';
-//     let destinatario = '';
+function Mensagem(dados) {
+    let classeMensagem = '';
+    let destinatario = '';
+    if (dados.type === 'status') {
+        classeMensagem = 'entrada-saida';
+    }
+    if (dados.type === 'private_message') {
+        if (dados.to !== nomeDeUsuario &&
+            dados.to !== "Todos" &&
+            dados.from !== nomeDeUsuario) {
+            return "";
+        }
+        classeMensagem = 'conversa-privada';
+        destinatario = `<span> para </span>
+        <strong>${dados.to}: </strong>`;
+    }
+    if (dados.type === 'message') {
+        classeMensagem = 'conversa-publica';
+        destinatario = `<span> para </span>
+        <strong>${dados.to}: </strong>`;
+    }
+    return `
+        <li class="${classeMensagem}" data-identifier="message">
+            <span class="horario">(${dados.time})</span>
+            <strong>${dados.from}</strong>
+            ${destinatario}
+            <span>${dados.text}</span>
+        </li>
+    `;
+}
 
-//     if (dados.type === 'status') {
-//         classeMensagem = 'entrada-saida';
-//     }
+function carregarMensagens() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promessa.then((resposta) => {
+        const mensagens = resposta.data;
+        const ulMensagens = document.querySelector(".mensagens-container");
+        ulMensagens.innerHTML = '';
+        for (let i = 0; i < mensagens.length; i++) {
+            ulMensagens.innerHTML += Mensagem(mensagens[i]);
+        }
+        scrollarAteUltimaMensagem();
+    });
+}
 
-//     if (dados.type === 'private_message') {
-//         if (dados.to !== nomeDeUsuario &&
-//             dados.to !== "Todos" &&
-//             dados.from !== nomeDeUsuario) {
-//             return "";
-//         }
+function perguntarNome() {
+    nomeDeUsuario = prompt("Qual seu lindo nome?");
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", {
+        name: nomeDeUsuario
+    });
+    promessa.then(() => {
+        setInterval(() => {
+            axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {
+                name: nomeDeUsuario
+            });
+        }, 4800);
+    });
+    promessa.catch(() => {
+        alert("Nome indisponivel");
+        perguntarNome();
+    });
+}
 
-//         classeMensagem = 'conversa-privada';
-//         destinatario = `<span> para </span>
-//         <strong>${dados.to}: </strong>`;
-//     }
+function iniciarApp() {
+    carregarMensagens();
+    setInterval(carregarMensagens, 3000);
+    perguntarNome();
+    const inputMensagem = document.querySelector('.input-mensagem');
+    inputMensagem.onkeydown = (e) => {
+        if (e.code === 'Enter') {
+            enviarMensagem();
+        }
+    };
+}
 
-//     if (dados.type === 'message') {
-//         classeMensagem = 'conversa-publica';
-//         destinatario = `<span> para </span>
-//         <strong>${dados.to}: </strong>`;
-//     }
+function enviarMensagem() {
+    const inputMensagem = document.querySelector('.input-mensagem');
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {
+        from: nomeDeUsuario,
+        to: "Todos",
+        text: inputMensagem.value,
+        type: "message"
+    });
+    inputMensagem.value = '';
+    promessa.then(carregarMensagens);
+    promessa.catch(() => {
+        window.location.reload();
+    });
+}
 
-//     return `
-//         <li class="${classeMensagem}" data-identifier="message">
-//             <span class="horario">(${dados.time})</span>
-//             <strong>${dados.from}</strong>
-//             ${destinatario}
-//             <span>${dados.text}</span>
-//         </li>
-//     `;
-// }
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const menuFundo = document.querySelector('.menu-fundo');
+    menu.classList.toggle("escondido");
+    menuFundo.classList.toggle("fundo-escondido");
+}
 
-// function carregarMensagens() {
-//     const promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-
-//     promessa.then((resposta) => {
-//         const mensagens = resposta.data;
-
-//         const ulMensagens = document.querySelector(".mensagens-container");
-//         ulMensagens.innerHTML = '';
-
-//         for (let i = 0; i < mensagens.length; i++) {
-//             ulMensagens.innerHTML += Mensagem(mensagens[i]);
-//         }
-
-//         scrollarAteUltimaMensagem();
-//     });
-// }
-
-// function perguntarNome() {
-//     nomeDeUsuario = prompt("Qual seu lindo nome?");
-
-//     const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", {
-//         name: nomeDeUsuario
-//     });
-
-//     promessa.then(() => {
-//         setInterval(() => {
-//             axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {
-//                 name: nomeDeUsuario
-//             });
-//         }, 4800);
-//     });
-
-//     promessa.catch(() => {
-//         alert("Nome indisponivel");
-//         perguntarNome();
-//     });
-// }
-
-// function iniciarApp() {
-//     carregarMensagens();
-//     setInterval(carregarMensagens, 3000);
-
-//     perguntarNome();
-
-//     const inputMensagem = document.querySelector('.input-mensagem');
-//     inputMensagem.onkeydown = (e) => {
-//         if (e.code === 'Enter') {
-//             enviarMensagem();
-//         }
-//     };
-// }
-
-// function enviarMensagem() {
-//     const inputMensagem = document.querySelector('.input-mensagem');
-
-//     const promessa = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", {
-//         from: nomeDeUsuario,
-//         to: "Todos",
-//         text: inputMensagem.value,
-//         type: "message"
-//     });
-
-//     inputMensagem.value = '';
-
-//     promessa.then(carregarMensagens);
-
-//     promessa.catch(() => {
-//         window.location.reload();
-//     });
-// }
-
-// function toggleMenu() {
-//     const menu = document.querySelector('.menu');
-//     const menuFundo = document.querySelector('.menu-fundo');
-
-//     menu.classList.toggle("escondido");
-//     menuFundo.classList.toggle("fundo-escondido");
-// }
-
-// iniciarApp();
-
+iniciarApp();
 
 
 
